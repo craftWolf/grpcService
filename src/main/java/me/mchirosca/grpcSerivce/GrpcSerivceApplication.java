dd4bc8eb35e5fcd00a6af53aa90837241d0cb832
mchirosca <mh.chirosca@protonmail.com>
2021-05-04 14:01:09 +0200

    Auto-generated project

---
+++ src/main/java/me/mchirosca/grpcSerivce/GrpcSerivceApplication.java
@@ -0,0 +1,13 @@
+package me.mchirosca.grpcSerivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSerivceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mchirosca/grpcSerivce/GrpcSerivceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mchirosca.grpcSerivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSerivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
